14ac15d8be0302acf8af60638dbe055636bc2fa3
Ginters Blauva <gisis26.01@inbox.lv>
2022-12-29 19:19:27 +0200

    Fix for formula arguments

--- app/Services/StravaHelperService.php
+++ app/Services/StravaHelperService.php
@@ -4,7 +4,6 @@ namespace App\Services;
 
 use App\Models\Strava_activity;
 use App\Models\User;
-use Illuminate\Support\Facades\Log;
 use DateTime;
 
 
@@ -114,7 +113,6 @@ class StravaHelperService
 
     public function isStravaUserAlreadyAssociated($strava_user_id){
         $users = User::where("strava_user_id",$strava_user_id)->get();
-
         return sizeof($users) > 0;
     }
 
